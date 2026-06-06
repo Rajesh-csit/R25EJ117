a1ba488e4bb0e1b4b503159e8a2506e13f6f497e
Rajesh R <rajeshrajeshrs5145269@gmail.com>
2026-06-06 19:35:39 +0530

    Day 3: Implemented rectangle, line, circle, triangle and delete function

--- graphics.c
+++ graphics.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "graphics.h"
 
 char canvas[ROWS][COLS];
@@ -29,5 +30,100 @@ void displayCanvas()
             printf("%c", canvas[i][j]);
         }
         printf("\n");
+    }
+}
+void drawRectangle(int x, int y, int width, int height)
+{
+    int i, j;
+
+    for(i = x; i < x + height; i++)
+    {
+        for(j = y; j < y + width; j++)
+        {
+            if(i >= 0 && i < ROWS && j >= 0 && j < COLS)
+            {
+                canvas[i][j] = '*';
+            }
+        }
+    }
+}
+
+void drawLine(int x1, int y1, int x2, int y2)
+{
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+
+    int sx = (x1 < x2) ? 1 : -1;
+    int sy = (y1 < y2) ? 1 : -1;
+
+    int err = dx - dy;
+
+    while(1)
+    {
+        if(x1 >= 0 && x1 < ROWS && y1 >= 0 && y1 < COLS)
+        {
+            canvas[x1][y1] = '*';
+        }
+
+        if(x1 == x2 && y1 == y2)
+            break;
+
+        int e2 = 2 * err;
+
+        if(e2 > -dy)
+        {
+            err -= dy;
+            x1 += sx;
+        }
+
+        if(e2 < dx)
+        {
+            err += dx;
+            y1 += sy;
+        }
+    }
+}
+
+void drawCircle(int xc, int yc, int r)
+{
+    int x, y;
+
+    for(x = 0; x < ROWS; x++)
+    {
+        for(y = 0; y < COLS; y++)
+        {
+            int d = (x - xc)*(x - xc) +
+                    (y - yc)*(y - yc);
+
+            if(abs(d - r*r) <= r)
+            {
+                canvas[x][y] = '*';
+            }
+        }
+    }
+}
+
+void drawTriangle(int x1, int y1,
+                  int x2, int y2,
+                  int x3, int y3)
+{
+    drawLine(x1, y1, x2, y2);
+    drawLine(x2, y2, x3, y3);
+    drawLine(x3, y3, x1, y1);
+}
+
+void deleteArea(int x, int y, int width, int height)
+{
+    int i, j;
+
+    for(i = x; i < x + height; i++)
+    {
+        for(j = y; j < y + width; j++)
+        {
+            if(i >= 0 && i < ROWS && j >= 0 && j < COLS)
+            {
+                canvas[i][j] = '_';
+            }
+        }
     }
 }
